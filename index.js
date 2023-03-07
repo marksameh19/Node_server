c178b36dbf786521341df50720c517050753e6cd
marksameh19 <43503794+marksameh19@users.noreply.github.com>
2023-03-07 10:30:39 +0200

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 const port = process.env.PORT || 3001;
 
 let notes = [
